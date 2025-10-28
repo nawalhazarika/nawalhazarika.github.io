745d8da4fa0233759a53cb472ca53ed50bd82168
nawalhazarika <77746521+nawalhazarika@users.noreply.github.com>
2025-10-27 22:27:11 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 
     function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("bdy").style.marginLeft = "300px";
-    document.getElementById("bdy").style.marginRight = "-250px";
+    document.getElementById("mySidenav").style.width = "210px";
+    document.getElementById("bdy").style.marginLeft = "240px";
+    document.getElementById("bdy").style.marginRight = "30px";
     /*document.getElementById("top").style.marginLeft = "260px";
     document.getElementById("top").style.marginRight = "-260px";*/
     }
@@ -19,6 +19,7 @@
     document.getElementById("bdy").className = "black";
     document.getElementById("ltsw").style = "display: flex;margin-left: 2%;color:white;text-decoration: none;cursor:pointer;font-size: 30px;";
     document.getElementById("drksw").style = "display: none;";
+    document.getElementById("assamese_name_message").style="color: #fbfbfb";
     localStorage.setItem("bdy", bdy);
     /*
     document.getElementById("ltsw").style = "position: absolute;top: 16px;right: 0px; font-size: 30px; margin-right: 5%; cursor:pointer;visibility:visible;color:white;display:block"*/
@@ -28,7 +29,11 @@
     function lightm() {
     document.getElementById("bdy").className = "bg";
     document.getElementById("ltsw").style = "display: none;";
-    document.getElementById("drksw").style = "display: flex;margin-left: 2%;color:rgb(31, 31, 31);text-decoration: none;cursor:pointer;font-size: 30px;";
+    document.getElementById("drksw").style = "display: flex;margin-left: 2%;color:rgba(27, 27, 27, 1);text-decoration: none;cursor:pointer;font-size: 30px;";
+    document.getElementById("assamese_name_message").style="color:rgba(27, 27, 27, 1)";
     localStorage.setItem("bdy", bdy);/*
     document.getElementById("drksw").style = "position: absolute;top: 16px;right: 0px; font-size: 30px; margin-right: 5%; cursor:pointer;visibility:visible;"*/
     }
+
+
+    
